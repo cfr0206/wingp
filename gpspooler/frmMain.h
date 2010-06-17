762c729b117c284d7e60d28db47670dedc2e3d2b
d.garkaev <d.garkaev@gmail.com>
2010-06-17 07:43:26 +0000

    - Add loging - Minimized on Exit - Popup tray menu - Add config file gpspooler.cfg

--- gpspooler/frmMain.h
+++ gpspooler/frmMain.h
@@ -14,43 +14,49 @@
 #include <FileCtrl.hpp>
 #include "frmJobFrame.h"
 #include <ScktComp.hpp>
+#include <Menus.hpp>
 
 //---------------------------------------------------------------------------
 class TfrmGPSpooler : public TForm
 {
 __published:	// IDE-managed Components
-    TTrayIcon *TrayIcon1;
+        TTrayIcon *TrayIcon;
     TPageControl *PageControl1;
     TTabSheet *tshJobs;
     TTabSheet *TabSheet2;
     TGroupBox *GroupBox1;
-        TPanel *Panel1;
-        TScrollBox *pnlJobs;
-        TPanel *Panel3;
-        TButton *btnSetEnv;
-        TButton *btnDelEnv;
-        TRadioButton *rbSystem;
-        TRadioButton *rbCurrentUser;
-        TMemo *mmEnv;
-        TServerSocket *IPCServer;
-        TSplitter *Splitter1;
-        TListBox *lbLog;
+    TPanel *Panel1;
+    TScrollBox *pnlJobs;
+    TPanel *Panel3;
+    TButton *btnSetEnv;
+    TButton *btnDelEnv;
+    TRadioButton *rbSystem;
+    TRadioButton *rbCurrentUser;
+    TMemo *mmEnv;
+    TServerSocket *IPCServer;
+        TCheckBox *cbMinOnExit;
+        TPopupMenu *TrayPopupMenu;
+        TMenuItem *cmdExit;
     void __fastcall btnSetEnvClick(TObject *Sender);
     void __fastcall btnDelEnvClick(TObject *Sender);
 
-        void __fastcall IPCServerClientRead(TObject *Sender,
-          TCustomWinSocket *Socket);
+    void __fastcall IPCServerClientRead(TObject *Sender,
+                                        TCustomWinSocket *Socket);
+        void __fastcall cbMinOnExitClick(TObject *Sender);
+        void __fastcall FormCloseQuery(TObject *Sender, bool &CanClose);
+        void __fastcall cmdExitClick(TObject *Sender);
 protected:	// User declarations
     AnsiString exe_name;
     AnsiString work_folder;
     AnsiString jobs_folder;
     AnsiString xml_folder;
     AnsiString temp_folder;
-    AnsiString exe_name_gprint;    
+    //AnsiString log_folder;
+    AnsiString exe_name_gprint;
     TStringList *printers;
     TFileListBox *job_list;
 
-    //TWatchFolder *WatchFolder;
+    bool ForcedClosure;
 
     void CheckJobFolder();
     void GetPrinterList(TStrings *);
@@ -60,6 +66,8 @@ protected:	// User declarations
     TfrmJobs * GetJobFrame(AnsiString jn);
     void MsgUpdateEnv();
     void AddJob(AnsiString job);
+    void ReadConfigParam();
+    void LogEnv();
 public:		// User declarations
     __fastcall TfrmGPSpooler(TComponent* Owner);
     __fastcall ~TfrmGPSpooler();
--- gpspooler/gpspooler.cpp
+++ gpspooler/gpspooler.cpp
@@ -4,49 +4,97 @@
 #include <ScktComp.hpp>
 #pragma hdrstop
 #include <windows.h>
+#include "uLogs.hpp"
+#include "globaldef.h"
+
+using namespace Ulogs;
 //---------------------------------------------------------------------------
 USEFORM("frmMain.cpp", frmGPSpooler);
 USEFORM("frmJobFrame.cpp", frmJobs); /* TFrame: File Type */
 //---------------------------------------------------------------------------
-WINAPI WinMain ( HINSTANCE, HINSTANCE, LPSTR lpCmdLine, int)
+
+void InitLogingSystem()
+{
+    AnsiString log_folder = ExtractFilePath ( Application->ExeName )+"log\\";
+    if ( !DirectoryExists ( log_folder ) )
+        CreateDir ( log_folder );
+
+    AnsiString logfilename;
+    TDateTime dt = Now();
+    AnsiString result;
+    DateTimeToString(result, "yyyymmddhhnnsszzz", dt);
+    logfilename=log_folder+result+".log";
+    TFileLog * FileLog = new TFileLog(TChannels()<<FL,logfilename);
+    SetLog(FileLog);
+}
+
+void SendNewJobName(AnsiString jobname)
+{
+    TClientSocket *IPCClient = new TClientSocket(NULL);
+    IPCClient->Port=37051;
+    IPCClient->Address="127.0.0.1";
+    IPCClient->ClientType = ctBlocking;
+    IPCClient->Active=true;
+    IPCClient->Socket->SendText(jobname);
+    IPCClient->Close();
+}
+
+void ReadConfig()
+{
+    AnsiString config_filename = ExtractFilePath ( Application->ExeName )+"gpspooler.cfg";
+    Config = new TStringList();
+    try
     {
+        Config->LoadFromFile(config_filename);
+    }
+    catch(EFOpenError *oe)
+    {
+        ToLog(FL,oe->Message);
+    }
+}
+
+void SaveConfig()
+{
+    AnsiString config_filename = ExtractFilePath ( Application->ExeName )+"gpspooler.cfg";
+    Config->SaveToFile(config_filename);
+}
+
+WINAPI WinMain ( HINSTANCE, HINSTANCE, LPSTR lpCmdLine, int)
+{
     HANDLE hMutex = CreateMutex(NULL, FALSE, "GPSPOOLER");
     if (WaitForSingleObject(hMutex,0)== WAIT_OBJECT_0)
     {
-    try
-        {
-        Application->Initialize();
-        Application->CreateForm(__classid(TfrmGPSpooler), &frmGPSpooler);
-        Application->Run();
-        }
-    catch ( Exception &exception )
-        {
-        Application->ShowException ( &exception );
-        }
-    catch ( ... )
-        {
+        InitLogingSystem();
+        ReadConfig();
         try
-            {
-            throw Exception ( "" );
-            }
+        {
+            Application->Initialize();
+            Application->CreateForm(__classid(TfrmGPSpooler), &frmGPSpooler);
+            Application->Run();
+            SaveConfig();
+        }
         catch ( Exception &exception )
-            {
+        {
             Application->ShowException ( &exception );
+        }
+        catch ( ... )
+        {
+            try
+            {
+                throw Exception ( "" );
+            }
+            catch ( Exception &exception )
+            {
+                Application->ShowException ( &exception );
             }
         }
-    ReleaseMutex(hMutex);    
+        ReleaseMutex(hMutex);
     }
     else //send job name
     {
-        TClientSocket *IPCClient = new TClientSocket(NULL);
-        IPCClient->Port=37051;
-        IPCClient->Address="127.0.0.1";
-        IPCClient->ClientType = ctBlocking;
-        IPCClient->Active=true;
-        IPCClient->Socket->SendText(lpCmdLine);
-        IPCClient->Close();
+        SendNewJobName(lpCmdLine);
     }
     CloseHandle(hMutex);
     return 0;
-    }
+}
 //---------------------------------------------------------------------------
